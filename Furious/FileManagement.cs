b5f63b91946de3f14ae06def84eff80fe8ca8e62
EzSxurce <48179642+Jxiced@users.noreply.github.com>
2020-06-15 17:27:46 +0100

    Implementation of async.

--- Furious/FileManagement.cs
+++ Furious/FileManagement.cs
@@ -1,40 +1,76 @@
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Furious
 {
 	internal class FileManagement
 	{
-		internal static void CleanFile(string path)
+		internal static string PTBPath
+		{
+			get
+			{
+				return FileManagement._ptbpath;
+			}
+			set
+			{
+				FileManagement._ptbpath = value;
+			}
+		}
+		internal static string DiscordPath
+		{
+			get
+			{
+				return FileManagement._discordpath;
+			}
+			set
+			{
+				FileManagement._discordpath = value;
+			}
+		}
+
+		internal static string CanaryPath
+		{
+			get
+			{
+				return FileManagement._canarypath;
+			}
+			set
+			{
+				FileManagement._canarypath = value;
+			}
+		}
+
+		internal static async Task CleanFile(string path)
 		{
 			try
 			{
 				using (StreamWriter streamWriter = new StreamWriter(path))
 				{
-					streamWriter.Write(string.Empty);
+					await streamWriter.WriteAsync(string.Empty);
 				}
 			}
 			catch
 			{
-				Debug.WriteLine("An error occured cleaning file.");
+				Debug.WriteLine("An error occured clearing index.js.");
 			}
 		}
 
-		internal static void WriteDiscord(string path)
+		internal static async Task WriteDiscord(string path)
 		{
 			if (File.Exists(FileManagement.DiscordPath))
 			{
 				try
 				{
-					string value;
+					string value = string.Empty;
 					using (StreamReader discordReader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Furious.Resources.index.txt")))
 					{
-						value = discordReader.ReadToEnd();
+						value = await discordReader.ReadToEndAsync();
 					}
 					using (StreamWriter discordWriter = new StreamWriter(path))
 					{
-						discordWriter.Write(value);
+						await discordWriter.WriteAsync(value);
 					}
 				}
 				catch
@@ -49,11 +85,11 @@ namespace Furious
 					string value;
 					using (StreamReader ptbWriter = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Furious.Resources.ptb.txt")))
 					{
-						value = ptbWriter.ReadToEnd();
+						value = await ptbWriter.ReadToEndAsync();
 					}
 					using (StreamWriter ptbWriter = new StreamWriter(path))
 					{
-						ptbWriter.Write(value);
+						await ptbWriter.WriteAsync(value);
 					}
 				}
 				catch
@@ -68,11 +104,11 @@ namespace Furious
 					string value;
 					using (StreamReader canaryReader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Furious.Resources.canary.txt")))
 					{
-						value = canaryReader.ReadToEnd();
+						value = await canaryReader.ReadToEndAsync();
 					}
 					using (StreamWriter canaryWriter = new StreamWriter(path))
 					{
-						canaryWriter.Write(value);
+						await canaryWriter.WriteAsync(value);
 					}
 				}
 				catch
@@ -82,25 +118,8 @@ namespace Furious
 			}
 		}
 
-		internal static string PTBPath
-		{
-			get { return FileManagement._ptbpath; }
-			set { FileManagement._ptbpath = value; }
-		}
-		internal static string DiscordPath
-		{
-			get { return FileManagement._discordpath; }
-			set { FileManagement._discordpath = value; }
-		}
-
-		internal static string CanaryPath
-		{
-			get { return FileManagement._canarypath; }
-			set { FileManagement._canarypath = value; }
-		}
-		
-		internal static string _ptbpath;
-		internal static string _discordpath;
-		internal static string _canarypath;
+		private static string _ptbpath;
+		private static string _discordpath;
+		private static string _canarypath;
 	}
 }
